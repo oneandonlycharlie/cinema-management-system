ffe5ec8405284c7ec55249e50718d44eb1c5d06d
Charlie <160505188+oneandonlycharlie@users.noreply.github.com>
2025-10-15 23:56:57 +0200

    Initial commit for spring boot setup

---
+++ src/main/java/com/cinema/cinema_backend/CinemaManagementSystemApplication.java
@@ -0,0 +1,16 @@
+package com.cinema.cinema_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CinemaManagementSystemApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CinemaManagementSystemApplication.class, args);
+		System.out.println("project started!");
+
+	}
+}
---
+++ src/test/java/com/cinema/cinema_backend/CinemaManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinema.cinema_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
