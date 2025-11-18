6fe718e5908bf4a33617cc43b4c662dede185e06
Charlie <160505188+oneandonlycharlie@users.noreply.github.com>
2025-11-18 18:02:14 +0100

    Add showtime mapper

---
+++ src/main/java/com/cinema/cinema_backend/dto/mapper/ShowTimeMapper.java
@@ -0,0 +1,28 @@
+package com.cinema.cinema_backend.dto.mapper;
+
+import com.cinema.cinema_backend.dto.ShowTimeDto;
+import com.cinema.cinema_backend.model.Seat;
+import com.cinema.cinema_backend.model.ShowTime;
+import org.springframework.security.core.userdetails.memory.UserAttribute;
+
+import java.util.stream.Collectors;
+
+public class ShowTimeMapper {
+    public static ShowTimeDto toDto(ShowTime showTime) {
+        ShowTimeDto dto = new ShowTimeDto();
+        dto.setId(showTime.getId());
+        dto.setHallId(showTime.getHall().getId());
+        dto.setFilmId(showTime.getFilm().getId());
+        dto.setStartTime(showTime.getStartTime());
+        dto.setEndTime(showTime.getEndTime());
+
+        dto.setSeatIds(
+                showTime.getSeats()
+                        .stream()
+                        .map(Seat::getId)
+                        .collect(Collectors.toList())
+        );
+
+        return dto;
+    }
+}
