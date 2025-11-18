0582ba897fca1575f52290b9bc17f40ae9aa1e37
Charlie <160505188+oneandonlycharlie@users.noreply.github.com>
2025-11-18 17:26:24 +0100

    Add function to auto adjust seats according to hall capacity

--- src/main/java/com/cinema/cinema_backend/service/SeatService.java
+++ src/main/java/com/cinema/cinema_backend/service/SeatService.java
@@ -1,5 +1,6 @@
 package com.cinema.cinema_backend.service;
 
+import com.cinema.cinema_backend.model.Hall;
 import com.cinema.cinema_backend.model.Seat;
 import com.cinema.cinema_backend.repository.SeatRepository;
 import org.springframework.stereotype.Service;
@@ -38,6 +39,26 @@ public class SeatService {
             return true;
         }
         return false;
+    }
+
+    public void createSeatsForHall(Hall hall, int amountToAdd){
+        for ( int i = 0; i < amountToAdd; i++){
+            Seat seat = new Seat();
+            seat.setHall(hall);
+            seatRepository.save(seat);
+            System.out.println("new seat added!" + seat);
+        }
+    }
+
+    public void deleteSeatsForHall(Hall hall, int amountToDelete) {
+        List<Seat> seats = seatRepository.findByHallIdOrderByIdDesc(hall.getId());
+        for (int i = 0; i < amountToDelete && i < seats.size(); i++) {
+            Seat seat = seats.get(i);
+            if (!seat.getTickets().isEmpty()) continue;
+            seatRepository.delete(seat);
+            System.out.println("seat deleted");
+        }
+
     }
 }
 
