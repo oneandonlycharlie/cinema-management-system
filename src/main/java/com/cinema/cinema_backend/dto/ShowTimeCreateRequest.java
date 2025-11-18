89af35eef12f64a4d40dee2b82420e04f01f5153
Charlie <160505188+oneandonlycharlie@users.noreply.github.com>
2025-11-18 18:03:44 +0100

    Add showtime create request

---
+++ src/main/java/com/cinema/cinema_backend/dto/ShowTimeCreateRequest.java
@@ -0,0 +1,55 @@
+package com.cinema.cinema_backend.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class ShowTimeCreateRequest {
+
+    private Long hallId;
+    private Long filmId;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private List<Long> seatIds;
+
+    // Getter / Setter
+    public Long getHallId() {
+        return hallId;
+    }
+
+    public void setHallId(Long hallId) {
+        this.hallId = hallId;
+    }
+
+    public Long getFilmId() {
+        return filmId;
+    }
+
+    public void setFilmId(Long filmId) {
+        this.filmId = filmId;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public List<Long> getSeatIds() {
+        return seatIds;
+    }
+
+    public void setSeatIds(List<Long> seatIds) {
+        this.seatIds = seatIds;
+    }
+}
+
