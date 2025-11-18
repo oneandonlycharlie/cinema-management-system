d5a5b3c0a7e4d872265f25a42be6be1cd6fcbb06
Charlie <160505188+oneandonlycharlie@users.noreply.github.com>
2025-11-18 17:26:48 +0100

    Add find seat by hall id function

--- src/main/java/com/cinema/cinema_backend/repository/SeatRepository.java
+++ src/main/java/com/cinema/cinema_backend/repository/SeatRepository.java
@@ -4,6 +4,9 @@ import com.cinema.cinema_backend.model.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SeatRepository extends JpaRepository<Seat, Long> {
+    List<Seat> findByHallIdOrderByIdDesc(Long hallId);
 }
