4e49f6c56d2131c0ffc5a1a58a185ed71f1316a2
Charlie <160505188+oneandonlycharlie@users.noreply.github.com>
2025-11-19 20:21:54 +0100

    Add wrapper

---
+++ src/main/java/com/cinema/cinema_backend/dto/mapper/FilmWrapper.java
@@ -0,0 +1,44 @@
+package com.cinema.cinema_backend.dto.mapper;
+
+import com.cinema.cinema_backend.dto.FilmDto;
+import com.cinema.cinema_backend.dto.ShowTimeDto;
+import com.cinema.cinema_backend.model.Actor;
+import com.cinema.cinema_backend.model.Film;
+import org.springframework.boot.actuate.endpoint.Show;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FilmWrapper {
+
+    public static FilmDto toDto(Film film){
+        List<String> actors = film.getActors()
+                .stream()
+                .map(Actor::getName)
+                .toList();
+        List<ShowTimeDto> showTimeDtos = film.getShowTimes()
+                .stream()
+                .map(showTime -> new ShowTimeDto(
+                        showTime.getId(),
+                        showTime.getHall().getId(),
+                        showTime.getHall().getName(),
+                        film.getId(),
+                        showTime.getStartTime(),
+                        showTime.getEndTime(),
+                        showTime.getSeatIds(),
+                        showTime.getPrice()
+                ))
+                .toList();
+        return new FilmDto(
+                film.getId(),
+                film.getName(),
+                film.getLength(),
+                film.getGenre(),
+                film.getIntro(),
+                film.getRating(),
+                showTimeDtos,
+                film.getDirector() != null ? film.getDirector().getName() : null,
+                actors
+        );
+    }
+}
