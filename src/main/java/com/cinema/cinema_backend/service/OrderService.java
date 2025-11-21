ad371c8f84626dab6541378704ab8e3448a8fc60
Charlie <160505188+oneandonlycharlie@users.noreply.github.com>
2025-11-22 00:53:12 +0100

    Add create order and pay for prder functions

--- src/main/java/com/cinema/cinema_backend/service/OrderService.java
+++ src/main/java/com/cinema/cinema_backend/service/OrderService.java
@@ -1,10 +1,19 @@
 package com.cinema.cinema_backend.service;
 
 
+import com.cinema.cinema_backend.dto.OrderCreateRequest;
+import com.cinema.cinema_backend.dto.OrderDto;
 import com.cinema.cinema_backend.dto.OrderUpdateRequest;
-import com.cinema.cinema_backend.model.Order;
+import com.cinema.cinema_backend.model.*;
+import com.cinema.cinema_backend.repository.CinemaUserRepository;
 import com.cinema.cinema_backend.repository.OrderRepository;
+import com.cinema.cinema_backend.repository.ShowTimeRepository;
+import com.cinema.cinema_backend.repository.TicketRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -14,15 +23,46 @@ import java.util.Optional;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final ShowTimeRepository showTimeRepository;
+    private final CinemaUserRepository cinemaUserRepository;
+    private final TicketService ticketService;
 
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(OrderRepository orderRepository, ShowTimeRepository showTimeRepository, CinemaUserRepository cinemaUserRepository, TicketService ticketService) {
         this.orderRepository = orderRepository;
+        this.showTimeRepository = showTimeRepository;
+        this.cinemaUserRepository = cinemaUserRepository;
+        this.ticketService = ticketService;
     }
 
     // Create
-    public Order save(Order order) {
-        return orderRepository.save(order);
+    @Transactional
+    public OrderDto createOrder(OrderCreateRequest request, Long userId) {
+        ShowTime showtime = showTimeRepository.findById(request.getShowtimeId())
+                .orElseThrow(() -> new RuntimeException("Showtime not found"));
+
+        CinemaUser user = cinemaUserRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        Order order = new Order();
+        order.setUser(user);
+        order.setStatus(OrderStatus.PENDING);
+        order.setCreatedAt(LocalDateTime.now());
+        order.setShowTime(showtime);
+        order.setSeatCount(request.getSeatCount());
+        order.setTotalAmount(showtime.getPrice().multiply(BigDecimal.valueOf(request.getSeatCount())));
+        orderRepository.save(order);
+
+        return new OrderDto(
+                order.getId(),
+                order.getStatus().name(),
+                order.getTotalAmount(),
+                order.getSeatCount(),
+                showtime.getFilm().getName(),
+                showtime.getStartTime(),
+                showtime.getHall().getName()
+        );
     }
+
+
 
     // Read all
     public List<Order> findAll() {
@@ -52,5 +92,33 @@ public class OrderService {
         }
         return false;
     }
+
+    @Transactional
+    public OrderDto payOrder(Long userId, Long orderId) {
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new RuntimeException("Order not found"));
+
+        if (!order.getUser().getId().equals(userId)) {
+            throw new RuntimeException("Unauthorized");
+        }
+
+        order.setStatus(OrderStatus.CONFIRMED);
+        orderRepository.save(order);
+
+        List<Ticket> tickets = ticketService.createTickets(order);
+
+        ShowTime showtime = order.getShowTime();
+
+        return new OrderDto(
+                order.getId(),
+                order.getStatus().name(),
+                order.getTotalAmount(),
+                order.getSeatCount(),
+                showtime.getFilm().getName(),
+                showtime.getStartTime(),
+                showtime.getHall().getName()
+        );
+    }
+
 }
 
