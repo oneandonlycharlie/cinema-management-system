0c288343655bf463a2afb52022f36418162714bc
Charlie <160505188+oneandonlycharlie@users.noreply.github.com>
2025-11-22 00:51:21 +0100

    Add find order by user id function

--- src/main/java/com/cinema/cinema_backend/repository/OrderRepository.java
+++ src/main/java/com/cinema/cinema_backend/repository/OrderRepository.java
@@ -4,7 +4,9 @@ import com.cinema.cinema_backend.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-
+    List<Order> findByCinemaUserId(Long userId);
 }
