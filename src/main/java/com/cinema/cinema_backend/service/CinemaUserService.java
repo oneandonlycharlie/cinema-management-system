429b205fb99be7dd1ec58bad035df4de2734c904
Charlie <160505188+oneandonlycharlie@users.noreply.github.com>
2025-11-22 00:47:33 +0100

    Add getMyOrders endpoint

--- src/main/java/com/cinema/cinema_backend/service/CinemaUserService.java
+++ src/main/java/com/cinema/cinema_backend/service/CinemaUserService.java
@@ -1,24 +1,31 @@
 package com.cinema.cinema_backend.service;
 
+import com.cinema.cinema_backend.dto.OrderWithTicketsDto;
+import com.cinema.cinema_backend.dto.mapper.OrderWrapper;
 import com.cinema.cinema_backend.dto.RegistrationRequest;
 import com.cinema.cinema_backend.model.CinemaUser;
+import com.cinema.cinema_backend.model.Order;
 import com.cinema.cinema_backend.repository.CinemaUserRepository;
+import com.cinema.cinema_backend.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class CinemaUserService {
     private final CinemaUserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final OrderRepository orderRepository;
 
     @Autowired
-    public CinemaUserService(CinemaUserRepository userRepository, PasswordEncoder passwordEncoder){
+    public CinemaUserService(CinemaUserRepository userRepository, PasswordEncoder passwordEncoder, OrderRepository orderRepository){
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.orderRepository = orderRepository;
     }
 
     public void register(RegistrationRequest request){
@@ -45,5 +52,13 @@ public class CinemaUserService {
 
     public List<CinemaUser> findAllUsers(){
         return userRepository.findAll();
+    }
+
+    public List<OrderWithTicketsDto> getMyOrders(Long userId) {
+        List<Order> orders = orderRepository.findByCinemaUserId(userId);
+
+        return orders.stream()
+                .map(OrderWrapper::toDto)
+                .collect(Collectors.toList());
     }
 }
