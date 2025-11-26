b37f95a2c72a3b3a85032e5e308b32af38c0d603
Charlie <160505188+oneandonlycharlie@users.noreply.github.com>
2025-11-26 14:19:18 +0100

    Add resource handler for images

---
+++ src/main/java/com/cinema/cinema_backend/config/StaticResourceConfig.java
@@ -0,0 +1,17 @@
+package com.cinema.cinema_backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class StaticResourceConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/images/**")
+                .addResourceLocations("file:uploads/films/");
+    }
+}
+
